f1bffbaddb65b6709102821b9700a8ffb531883a
jetenergy <renegrapjas@hotmail.com>
2020-05-28 12:01:24 +0200

    Removed feature that wasnt supposed to be here

--- Hivemind/Assets/Scripts/BaseBuilding/BaseController.cs
+++ Hivemind/Assets/Scripts/BaseBuilding/BaseController.cs
@@ -43,11 +43,6 @@ public class BaseController : MonoBehaviour
 
     void Update()
     {
-        if (true) // TODO: if you are the owner of the base
-        {
-            //Highlight();
-        }
-
         if (Input.GetButtonDown("Fire1") && !EventSystem.current.IsPointerOverGameObject())
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -76,62 +71,6 @@ public class BaseController : MonoBehaviour
     private void OnDestroy()
     {
         CancelInvoke("VerifyBuildingTasks");
-    }
-
-    GameObject highlight;
-    private void Highlight()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit, 100.0f, LayerMask))
-        {
-            GameObject plate;
-            if (hit.transform.gameObject.layer == UnityEngine.LayerMask.NameToLayer("BaseFloor"))
-            {
-                plate = hit.transform.gameObject;
-            }
-            else
-            {
-                plate = hit.transform.parent.gameObject;
-            }
-
-            var baseTile = plate.GetComponent<BaseTile>();
-
-            if ((HighlightedTile == null || baseTile != HighlightedTile) && highlight == null)
-            {
-                if (baseTile.CurrTile == null)
-                {
-                    return;
-                    highlight = Instantiate(new GameObject());
-                    highlight.AddComponent<MeshRenderer>();
-                    highlight.GetComponent<MeshRenderer>().material = baseTile.GetComponent<MeshRenderer>().material;
-                    highlight.AddComponent<MeshFilter>();
-                    highlight.GetComponent<MeshFilter>().sharedMesh = Instantiate(baseTile.GetComponent<MeshFilter>().sharedMesh);
-
-                    //highlight = Instantiate(baseTile.gameObject.GetComponent<MeshRenderer>().sharedMaterial;
-                }
-                else
-                {
-                    highlight = Instantiate(baseTile.CurrTile);
-                }
-
-                HighlightedTile = baseTile;
-                highlight.transform.SetParent(baseTile.transform, false);
-                highlight.transform.localPosition += new Vector3(0, 4, 0);
-            }
-            else if (HighlightedTile != null && baseTile != HighlightedTile || baseTile == null)
-            {
-                Destroy(highlight);
-                HighlightedTile = null;
-            }
-        }
-        else
-        {
-            if (highlight != null)
-            {
-                Destroy(highlight);
-                HighlightedTile = null;
-            }
-        }
     }
 
     private void OnLeftClick(BaseTile tile)
