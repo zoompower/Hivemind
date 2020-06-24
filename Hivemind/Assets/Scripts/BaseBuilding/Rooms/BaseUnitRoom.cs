356a4f89309907f9e9166b97d7a6a0155230c2d0
jetenergy <renegrapjas@hotmail.com>
2020-06-24 13:44:56 +0200

    Different fix for spawn units when game loaded

--- Hivemind/Assets/Scripts/BaseBuilding/Rooms/BaseUnitRoom.cs
+++ Hivemind/Assets/Scripts/BaseBuilding/Rooms/BaseUnitRoom.cs
@@ -112,9 +112,8 @@ public abstract class BaseUnitRoom : BaseRoom
 
         if (!Parent.Loaded)
         {
-            //singleFree = true;
+            singleFree = true;
         }
-        baseController.GetGameResources().AddResources(RespawnCost);
     }
 
     internal void AttachUnitGroup()
