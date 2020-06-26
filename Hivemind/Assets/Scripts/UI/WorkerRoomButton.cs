20a687d5e95e1c84afde8f7e7113aa83c314960a
Eriklouwerse <eriklouwerse1@gmail.com>
2020-06-26 13:12:36 +0200

    Updating workerroom remaining WIP

---
+++ Hivemind/Assets/Scripts/UI/WorkerRoomButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorkerRoomButton : MonoBehaviour
+{
+    [SerializeField]
+    private Text myText;
+
+    private void Start()
+    {
+        //eventsomewhereidk += UpdateText;
+    }
+
+    private void UpdateText(object sender, int args)
+    {
+        myText.text = $"Worker Room \n(Costs 10 rocks) \n({50 - args} Remaining)";
+    }
+}
