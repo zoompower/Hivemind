f24c36904e834e026bf5569ce62bb65d1629c558
Eriklouwerse <eriklouwerse1@gmail.com>
2020-06-26 15:44:02 +0200

    Change button interactable based on how many unitrooms you have

--- Hivemind/Assets/Scripts/UI/WorkerRoomButton.cs
+++ Hivemind/Assets/Scripts/UI/WorkerRoomButton.cs
@@ -9,13 +9,27 @@ public class WorkerRoomButton : MonoBehaviour
     [SerializeField]
     private Text myText;
 
+    [SerializeField]
+    private Button myButton;
+
+    private BaseController myBaseController;
+
     private void Start()
     {
+        myBaseController = GameWorld.Instance.BaseControllerList.Where(controller => controller.TeamID == GameWorld.Instance.LocalTeamId).FirstOrDefault();
         GameWorld.Instance.UnitControllerList.Where(controller => controller.TeamId == GameWorld.Instance.LocalTeamId).FirstOrDefault().MindGroupList.OnAmountGet += UpdateText;
     }
 
     private void UpdateText(object sender, AmountChangedEventArgs args)
     {
         myText.text = $"Worker Room \n(Costs 10 rocks) \n({GameWorld.UnitLimit - args.Amount} Remaining)";
+        if(args.Amount >= GameWorld.UnitLimit)
+        {
+            myButton.interactable = false;
+        }
+        else if(myBaseController.GetGameResources().GetResourceAmounts()[ResourceType.Rock] >= 10)
+        {
+            myButton.interactable = true;
+        }
     }
 }
